a7c1b3512fc90d193ce719fddc3c01c9f09b6acb
parcok <parcok@sheridancollege.ca>
2017-07-12 14:32:02 -0400

    With sleep delay only 3rd file worked.

--- NavExcel/NavExcel/Form1.Designer.cs
+++ NavExcel/NavExcel/Form1.Designer.cs
@@ -30,6 +30,9 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -62,14 +65,14 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(141, 215);
+            this.progressBar1.Location = new System.Drawing.Point(141, 243);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(308, 23);
             this.progressBar1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(16, 75);
+            this.textBox2.Location = new System.Drawing.Point(16, 123);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(433, 104);
@@ -77,7 +80,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(34, 215);
+            this.button2.Location = new System.Drawing.Point(34, 243);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
@@ -85,11 +88,42 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(115, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(253, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(374, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Browse...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Template:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 262);
+            this.ClientSize = new System.Drawing.Size(470, 286);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.progressBar1);
@@ -99,7 +133,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "NavExcel";
+            this.Text = "Student Lesson Plan Creator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,6 +147,9 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- NavExcel/NavExcel/Form1.cs
+++ NavExcel/NavExcel/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
-using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
 using System.Collections;
 using System.IO;
 
@@ -18,118 +18,76 @@ namespace NavExcel {
             InitializeComponent();
         }
         string path = "";
+        string template = "";
 
+        // Browse for folder
         private void button1_Click(object sender, EventArgs e) {
+            path = "\\\\central\\ops\\OperationalTraining\\YYZ\\2 Generic Training\\Kevin Testing (Please Don't Touch)\\";
+            //path = "D:\\Temp2\\";
             using (var folderDialog = new FolderBrowserDialog()) {
                 if (folderDialog.ShowDialog() == DialogResult.OK) {
                     path = folderDialog.SelectedPath;
                     textBox1.Text = path;
                 }
             }
+            textBox1.Text = path;
+        }
+
+        // Browse for template
+        private void button3_Click(object sender, EventArgs e) {
+            using (var fileSelection = new OpenFileDialog()) {
+                fileSelection.Filter = "Word Templates | *.dotm";
+                if (fileSelection.ShowDialog() == DialogResult.OK) {
+                    template = fileSelection.FileName;
+                    textBox3.Text = template;
+                }
+            }
         }
 
         private void button2_Click(object sender, EventArgs e) {
-            if (textBox1.Text == "") {
+            if (path == "") {
                 MessageBox.Show("Please select the root folder with the browse button.");
+            /*} else if (template == "") {
+                MessageBox.Show("Please select the template you wish to apply.");*/
             } else {
-                string[] excelFiles = Directory.GetFiles(path, "*", SearchOption.AllDirectories).Where(s => s.EndsWith(".xls") || s.EndsWith(".xlsx")).ToArray();
-                int fileAmount = excelFiles.Length;
+                string[] wordFiles = Directory.GetFiles(path, "*", SearchOption.AllDirectories).Where(s => s.EndsWith(".doc") || s.EndsWith(".docx")).ToArray();
+                int fileAmount = wordFiles.Length;
                 progressBar1.Maximum = fileAmount;
-                foreach (string file in excelFiles) {
+                foreach (string file in wordFiles) {
                     try {
 
-                        //Console.Write("FILE: " + file + " - ");
-                        Excel.Application excel = new Excel.Application();
-                        Excel.Workbook workbook = excel.Workbooks.Open(file);
-                        Excel.Worksheet airSheet = excel.Sheets[1];
-                        Excel.Worksheet groundSheet = excel.Sheets[2];
-                        excel.DisplayAlerts = false;
+                        object missing = System.Reflection.Missing.Value;
+                        Word.Application wordApp = new Word.ApplicationClass();
+                        Word.Document aDoc = null;
 
-                        // Cells go down then across
-                        if ((string)airSheet.Cells[4, 1].value2 == "TIME") {
-                            // THE WORKING SOLUTION TO FIND THE AMOUNT OF USED ROWS
-                            int airRowAmount = airSheet.Cells.Find("*", System.Reflection.Missing.Value, Excel.XlFindLookIn.xlValues, Excel.XlLookAt.xlWhole, Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious, false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
-                            int groundRowAmount = groundSheet.Cells.Find("*", System.Reflection.Missing.Value, Excel.XlFindLookIn.xlValues, Excel.XlLookAt.xlWhole, Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious, false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
-                            // END OF THE WORKING SOLUTION TO FIND THE RIGHT AMOUNT OF USED ROWS
+                        wordApp.Visible = false;
+                        wordApp.DisplayAlerts = Word.WdAlertLevel.wdAlertsNone;
+                        aDoc = wordApp.Documents.Add(template/*template*/, ref missing/*new template*/, ref missing/*doc type*/, false/*visible*/);
 
+                        aDoc = wordApp.Documents.Open(file/*filename*/, false/*convert file prompt*/,
+                                                      false/*readonly*/, false/*recent files*/, ref missing/*read pass*/,
+                                                      ref missing/*read template pass*/, true/*reopen*/, ref missing/*write pass*/,
+                                                      ref missing/*write template pass*/, ref missing/*format*/, ref missing/*encoding*/,
+                                                      false/*visible client*/, ref missing/*repair*/, ref missing/*direction*/,
+                                                      ref missing/*NoEncodingDialog*/, ref missing/*XMLTransform*/);
+                        System.Threading.Thread.Sleep(30000);
+                        //Console.WriteLine(aDoc.Name);
+                        wordApp.Documents.CheckOut(file);
+                        System.Threading.Thread.Sleep(30000);
+                        //aDoc.Activate();
+                        attachTemplate(aDoc, template);
 
-                            // Add colour to the tables
-                            Excel.Range airTable = airSheet.Range["A4:G" + airRowAmount];
-                            airTable.Cells.Interior.Color = System.Drawing.ColorTranslator.FromHtml("#C6E2E9"); // MATCH WITH AIRSHEET
-                            groundSheet.Cells.Interior.Color = System.Drawing.ColorTranslator.FromHtml("#F1FFC4"); // MATCH WITH TITLERANGE
-
-                            // Copy from ground to air
-                            string rangeToCopy = "A4:G" + groundRowAmount;
-                            Excel.Range rangeFrom = groundSheet.Range[rangeToCopy];
-                            Excel.Range rangeTo = airSheet.Range["A" + (airRowAmount + 2)];
-                            rangeFrom.Copy(rangeTo);
-
-                            // Title for ground
-                            airSheet.Range["A" + (airRowAmount + 1)].Cells.Font.Size = 36;
-                            airSheet.Range["A" + (airRowAmount + 1)].Value2 = "GROUND";
-                            Excel.Range titleRange = airSheet.Range["A" + (airRowAmount + 1) + ":G" + (airRowAmount + 1)];
-                            MergeAndCenter(titleRange);
-                            titleRange.Cells.Interior.Color = System.Drawing.ColorTranslator.FromHtml("#F1FFC4"); // MATCH WITH GROUNDSHEET
-                            titleRange.Cells.Font.Bold = true;
-
-                            // Get the total amount of rows
-                            int totalRowAmount = airSheet.Cells.Find("*", System.Reflection.Missing.Value, Excel.XlFindLookIn.xlValues, Excel.XlLookAt.xlWhole, Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious, false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
-
-                            // Need to get the range of the data we copied over, isolate the vehicles, then sort again
-                            Excel.Range groundTable = airSheet.Range["A" + (airRowAmount + 3) + ":G" + totalRowAmount];
-                            groundTable.Sort(groundTable.Columns[3, Type.Missing], Excel.XlSortOrder.xlDescending, groundTable.Columns[3, Type.Missing], Type.Missing, Excel.XlSortOrder.xlAscending, Type.Missing, Excel.XlSortOrder.xlAscending, Excel.XlYesNoGuess.xlYes, Type.Missing, Type.Missing, Excel.XlSortOrientation.xlSortColumns, Excel.XlSortMethod.xlPinYin, Excel.XlSortDataOption.xlSortNormal, Excel.XlSortDataOption.xlSortNormal, Excel.XlSortDataOption.xlSortNormal);
-
-                            int vehicleAmount = 0;
-                            Excel.Range vehicleGrab = airSheet.Range["C" + (airRowAmount + 3) + ":C" + totalRowAmount];
-                            foreach (Excel.Range item in vehicleGrab.Cells) {
-                                if (item.Text == "") {
-                                    vehicleAmount++;
-                                }
-                            }
-
-                            // Only if there are vehicles
-                            if (vehicleAmount > 0) {
-                                Excel.Range sortNonVehicles = airSheet.Range["A" + (airRowAmount + 3) + ":G" + (totalRowAmount - vehicleAmount)];
-                                sortNonVehicles.Sort(groundTable.Columns[1, Type.Missing], Excel.XlSortOrder.xlAscending, groundTable.Columns[1, Type.Missing], Type.Missing, Excel.XlSortOrder.xlAscending, Type.Missing, Excel.XlSortOrder.xlAscending, Excel.XlYesNoGuess.xlYes, Type.Missing, Type.Missing, Excel.XlSortOrientation.xlSortColumns, Excel.XlSortMethod.xlPinYin, Excel.XlSortDataOption.xlSortNormal, Excel.XlSortDataOption.xlSortNormal, Excel.XlSortDataOption.xlSortNormal);
-
-                                Excel.Range sortVehicles = airSheet.Range["A" + (totalRowAmount - vehicleAmount + 1) + ":G" + totalRowAmount];
-                                sortVehicles.Cells.Interior.Color = System.Drawing.ColorTranslator.FromHtml("#FFCAAF");
-                                sortVehicles.Sort(groundTable.Columns[1, Type.Missing], Excel.XlSortOrder.xlAscending, groundTable.Columns[1, Type.Missing], Type.Missing, Excel.XlSortOrder.xlAscending, Type.Missing, Excel.XlSortOrder.xlAscending, Excel.XlYesNoGuess.xlYes, Type.Missing, Type.Missing, Excel.XlSortOrientation.xlSortColumns, Excel.XlSortMethod.xlPinYin, Excel.XlSortDataOption.xlSortNormal, Excel.XlSortDataOption.xlSortNormal, Excel.XlSortDataOption.xlSortNormal);
-                            }
-
-                            // Title for tower
-                            Excel.Range towerTitle = (Excel.Range)airSheet.Rows[4];
-                            towerTitle.Insert();
-                            airSheet.Range["A4"].Cells.Font.Size = 36;
-                            airSheet.Range["A4"].Value2 = "TOWER";
-                            towerTitle = airSheet.Range["A4:G4"];
-                            MergeAndCenter(towerTitle);
-                            towerTitle.Cells.Interior.Color = System.Drawing.ColorTranslator.FromHtml("#C6E2E9");
-                            towerTitle.Cells.Font.Bold = true;
-
-                            // Fix borders
-                            Excel.Range fixBorders = airSheet.Range["A4:G" + (totalRowAmount + 1)];
-                            fixBorders.Cells.Borders.Weight = Excel.XlBorderWeight.xlThick;// (Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick, Excel.XlColorIndex.xlColorIndexAutomatic, System.Drawing.Color.Black, System.Drawing.Color.Black);
-
-                            airSheet.Sort.SortFields.Clear();
-
-                            airSheet.Columns.AutoFit();
-                            airSheet.Rows.AutoFit();
-
-                            airSheet.PageSetup.PrintArea = "A1:G" + (totalRowAmount + 1);
-
-                            excel.DisplayAlerts = false;
-                            if (file.EndsWith(".xls")) {
-                                workbook.SaveAs(file + "x", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Excel.XlSaveAsAccessMode.xlNoChange, Excel.XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
-                            } else {
-                                workbook.SaveAs(file, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Excel.XlSaveAsAccessMode.xlNoChange, Excel.XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
-                            }
-
+                        aDoc.SaveAs(file/*filename*/, ref missing/*file format*/, ref missing/*lock comments*/,
+                                    ref missing/*pass*/, ref missing/*recent files*/, ref missing/*write pass*/,
+                                    false/*read only suggest*/, ref missing/*embed fonts*/, ref missing/*native pic format*/,
+                                    ref missing/*form data*/, ref missing/*AOCE letter*/, ref missing/*encoding*/,
+                                    ref missing/*line breaks*/, ref missing/*substitutions*/, ref missing/*line endigng*/, ref missing/*BiDi marks*/);
+                        if (aDoc.CanCheckin()) {
+                            aDoc.CheckIn();
+                        } else {
+                            textBox2.Text += "Could not check in " + file + "\r\n";
                         }
-                        workbook.Sheets[2].delete();
-                        workbook.Sheets[2].delete();
-                        workbook.Close(true, Type.Missing, Type.Missing);
-                        excel.Quit();
+                        aDoc.Close(Word.WdSaveOptions.wdSaveChanges, ref missing, ref missing);
 
                     } catch {
                         textBox2.Text += "Couldn't open " + file + "\r\n";
@@ -137,8 +95,8 @@ namespace NavExcel {
                     progressBar1.Value++;
                     progressBar1.Update();
                 }
-                Process[] excelClients = Process.GetProcessesByName("excel");
-                foreach (Process p in excelClients) {
+                Process[] wordClients = Process.GetProcessesByName("winword");
+                foreach (Process p in wordClients) {
                     p.Kill();
                 }
                 if (textBox2.Text == "") {
@@ -149,20 +107,11 @@ namespace NavExcel {
             }
         }
 
-        public static void MergeAndCenter(Excel.Range MergeRange) {
-            MergeRange.Select();
-
-            MergeRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
-            MergeRange.VerticalAlignment = Excel.XlVAlign.xlVAlignBottom;
-            MergeRange.WrapText = false;
-            MergeRange.Orientation = 0;
-            MergeRange.AddIndent = false;
-            MergeRange.IndentLevel = 0;
-            MergeRange.ShrinkToFit = false;
-            MergeRange.ReadingOrder = (int)(Excel.Constants.xlContext);
-            MergeRange.MergeCells = false;
-
-            MergeRange.Merge(System.Type.Missing);
+        private static void attachTemplate(Word.Document document, string templateFilePath) {
+            object oTemplate = (object)templateFilePath;
+            document.set_AttachedTemplate(ref oTemplate);
+            document.UpdateStyles();
+            MessageBox.Show("Successfully attached template.\r\n"); 
         }
     }
 }
